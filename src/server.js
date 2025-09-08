79ad541c99409e32d32f30864b7000ab3c6ddada
Anastasiia <lobash.n@gmail.com>
2025-09-08 20:06:27 +0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -26,7 +26,10 @@ export const startServer = () => {
   app.use(express.json());
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: getEnvVar(
+        'https://tasteorama.vercel.app/',
+        'http://localhost:5173',
+      ),
       credentials: true,
     }),
   );
